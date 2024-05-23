91ad70ec01ca8ea9f8b8c8d363bc0cc93e2d6bc8
Jean Nunes <jean.nunes@uzzipay.com>
2024-05-23 15:07:53 -0400

    Adjust fonts

--- src/styles/mixins.ts
+++ src/styles/mixins.ts
@@ -4,44 +4,44 @@ export const mixins = {
   fonts: {
     titleL: css`
       font-family: "Nunito", sans-serif;
-      font-size: 1.5;
+      font-size: 1.5rem;
       line-height: 130%;
-      font-weight: 500;
+      font-weight: 700;
     `,
 
     titleM: css`
       font-family: "Nunito", sans-serif;
       font-size: 1.25rem;
       line-height: 160%;
-      font-weight: 500;
+      font-weight: 700;
     `,
 
     titleS: css`
       font-family: "Nunito", sans-serif;
       font-size: 1.125rem;
       line-height: 160%;
-      font-weight: 500;
+      font-weight: 700;
     `,
 
     textM: css`
-      font-family: "Roboto", sans-serif;
+      font-family: "Nunito", sans-serif;
       font-size: 1rem;
       line-height: 160%;
       font-weight: 400;
     `,
 
     textS: css`
-      font-family: "Roboto", sans-serif;
+      font-family: "Nunito", sans-serif;
       font-size: 0.875rem;
       line-height: 160%;
       font-weight: 400;
     `,
 
     link: css`
-      font-family: "Roboto", sans-serif;
+      font-family: "Nunito", sans-serif;
       font-size: 0.75rem;
       line-height: 160%;
-      font-weight: 400;
+      font-weight: 700;
     `,
   },
 };
